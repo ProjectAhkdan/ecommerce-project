6f9c6d6b0fbe87b0b893646a7c60820e25a25816
ProjectAhkdan <ahkdanprika1@gmail.com>
2025-06-01 14:45:38 +0700

    update providers

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,24 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-})
+});
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')]
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+    plugins: ['@typescript-eslint', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  }),
+];
 
-export default eslintConfig
+export default eslintConfig;
